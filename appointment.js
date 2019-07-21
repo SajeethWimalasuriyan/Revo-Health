0aba6d2e0131ddadf63cbfd675ecd528d70f3f31
Parmvir Grewal <parmg100@gmail.com>
2019-07-21 15:14:50 -0400

    Add theme switcher and general changes.

--- appointment.js
+++ appointment.js
@@ -5,8 +5,23 @@ function my_app_button(){
     document.getElementById("a3").style.backgroundColor = "#5856d6";
     document.getElementById("a4").style.backgroundColor = "#5856d6";
     document.getElementById("a4").style.backgroundColor = "#5856d6";
+    document.li.li.backgroundColor = "#5856d6";
     document.getElementById("nav1").style.borderBottomColor = "#5856d6";
     document.getElementById("nav2").style.borderBottomColor = "#5856d6";
 
+    x=document.getElementsByClassName("choose_appointment");  // Find the elements
+    for(var i = 0; i < x.length; i++){
+    x[i].innerText="Reschedule/Cancel";    // Change the content
+    }
+}
+function theme(){
+
+    document.body.style.backgroundColor = "white";
+    document.getElementById("customer_name").style.color = "black";
+    document.getElementById("app_avail").style.color = "black";
+    document.getElementById("nav1").style.color = "black";
+    document.getElementById("appointment_check").style.color = "black";
+    document.getElementById("theme").style.color = "black";
+    document.getElementById("logout_button").style.color = "black";
 
 }
