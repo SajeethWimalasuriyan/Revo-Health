672cec2577070661fb7f221929b74307fd1ccf86
sajeeth wimalasuriyan <sajeethwim2579@gmail.com>
2019-07-20 20:49:01 -0400

    Clean code, tweak code and add basic JS.

---
+++ appointment.js
@@ -0,0 +1,12 @@
+function my_app_button(){
+    document.getElementById("app_avail").innerHTML = "Your Current Appointments:"; 
+    document.getElementById("a1").style.backgroundColor = "#5856d6";
+    document.getElementById("a2").style.backgroundColor = "#5856d6";
+    document.getElementById("a3").style.backgroundColor = "#5856d6";
+    document.getElementById("a4").style.backgroundColor = "#5856d6";
+    document.getElementById("a4").style.backgroundColor = "#5856d6";
+    document.getElementById("nav1").style.borderBottomColor = "#5856d6";
+    document.getElementById("nav2").style.borderBottomColor = "#5856d6";
+
+
+}
